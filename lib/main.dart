d71411f64ae96eb40b0c6fe536f407044bcb40ed
RitutoshBhatia <ritutosh@aeologic.com>
2026-08-12 12:17:23 +0530

    Locked the screen orientation to Portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app/app.dart';
 import 'core/utils/logger.dart';
@@ -8,6 +9,12 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  // Lock device orientation to portrait mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   // Initialize GetStorage for fast local persistence
   await GetStorage.init();
   
